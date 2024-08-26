949851e7c6e45f1ffdadf968b6a01d4a732e781b
GuilhermeNight <00001079608941sp@al.educacao.sp.gov.br>
2024-08-26 09:51:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+  background("pink");
+}
 
+function draw() {
+
+  stroke("white")
+  fill("black")
+  
+  if (mouseIsPressed){
+    rect (mouseX,mouseY, 20,30);
+  }
+}  
